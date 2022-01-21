a5c50475adb26d31a98d6552a0db9bdc45fe0cf3
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-21 10:49:05 +0900

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import Link from "next/link"
+
+function Header() {
+  const defaultImage: string = "https://links.papareact.com/yvf"
+
+  return (
+    <header className="flex justify-between p-4 mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href='/'>
+          <img
+            src='/images/NESHOT (1).png'
+            className="w-32 md:w-44 -mt-1 object-contain cursor-pointer"
+          />
+        </Link>
+
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-teal-600 rounded-full px-4 py-1">
+            Follow
+          </h3>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-5 text-black">
+        <h3>Sign In</h3>
+        <h3 className="hidden border px-2 py-2 rounded-full border-black">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
